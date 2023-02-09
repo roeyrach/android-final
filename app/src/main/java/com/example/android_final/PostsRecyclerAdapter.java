b9fe555da6b50fe940c79d0efe96ecec2cd98e29
roeyrach <93585090+roeyrach@users.noreply.github.com>
2023-02-09 19:43:46 +0200

    posts, adapter, recyclers

---
+++ app/src/main/java/com/example/android_final/PostsRecyclerAdapter.java
@@ -0,0 +1,89 @@
+package com.example.android_final;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.android_final.model.Post;
+
+import java.util.List;
+
+class PostViewHolder extends RecyclerView.ViewHolder {
+    TextView userName;
+    TextView postTextContent;
+    List<Post> data;
+    ImageView avatarImage;
+
+    public PostViewHolder(@NonNull View itemView, PostRecyclerAdapter.OnItemClickListener listener, List<Post> data) {
+        super(itemView);
+        this.data = data;
+        userName = itemView.findViewById(R.id.userName);
+        postTextContent = itemView.findViewById(R.id.postTextContent);
+//        avatarImage = itemView.findViewById(R.id.);
+        itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int pos = getAdapterPosition();
+                if (listener != null) {
+                    listener.onItemClick(pos);
+                }
+            }
+        });
+    }
+
+    public void bind(Post post, int pos) {
+        userName.setText(post.getUserName());
+        postTextContent.setText(post.getPostTextContent());
+    }
+}
+
+
+class PostRecyclerAdapter extends RecyclerView.Adapter<PostViewHolder> {
+    OnItemClickListener listener;
+
+    public static interface OnItemClickListener {
+        void onItemClick(int pos);
+    }
+
+    LayoutInflater inflater;
+    List<Post> data;
+
+    public void setData(List<Post> data) {
+        this.data = data;
+        notifyDataSetChanged();
+    }
+
+    public PostRecyclerAdapter(LayoutInflater inflater, List<Post> data) {
+        this.inflater = inflater;
+        this.data = data;
+    }
+
+    void setOnItemClickListener(OnItemClickListener listener) {
+        this.listener = listener;
+    }
+
+    @NonNull
+    @Override
+    public PostViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.post_list_row, parent, false);
+        PostViewHolder holder = new PostViewHolder(view, listener, data);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PostViewHolder holder, int position) {
+        Post post = data.get(position);
+        holder.bind(post, position);
+    }
+
+    @Override
+    public int getItemCount() {
+        if (data == null) return 0;
+        return data.size();
+    }
+}
--- app/src/main/java/com/example/android_final/model/Post.java
+++ app/src/main/java/com/example/android_final/model/Post.java
@@ -1,5 +1,68 @@
 package com.example.android_final.model;
 
-public class Post {
 
+public class Post {
+    public String postId = "";
+    public String avatarUrl = "";
+    public String userName = "";
+    public String postTextContent = "";
+    public String imageUrl = "";
+    public Long lastUpdated;
+
+    public Post() {
+    }
+
+    public Post(String userName, String postTextContent, String imageUrl) {
+        this.userName = userName;
+        this.postTextContent = postTextContent;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getPostId() {
+        return postId;
+    }
+
+    public void setPostId(String postId) {
+        this.postId = postId;
+    }
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    public void setAvatarUrl(String avatarUrl) {
+        this.avatarUrl = avatarUrl;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPostTextContent() {
+        return postTextContent;
+    }
+
+    public void setPostTextContent(String postTextContent) {
+        this.postTextContent = postTextContent;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Long getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated(Long lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
 }
