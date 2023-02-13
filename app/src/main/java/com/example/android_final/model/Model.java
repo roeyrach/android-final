fccc31f4befb91c42df4bb6aec1fc0bf325bf834
roeyrach <93585090+roeyrach@users.noreply.github.com>
2023-02-13 19:09:58 +0200

    Revert "Merge branch 'main' into Roey"
    This reverts commit c126c0312ee67f59c95d89e20c1b6f3660e24f96, reversing
    changes made to 0cac5244dd2d4aeda8b0272e3aedf7d5c67b5aed.

--- app/src/main/java/com/example/android_final/model/Model.java
+++ app/src/main/java/com/example/android_final/model/Model.java
@@ -1,15 +1,9 @@
 package com.example.android_final.model;
 
-
 import android.os.Handler;
 import android.os.Looper;
 
 import androidx.core.os.HandlerCompat;
-import android.util.Log;
-
-import com.google.firebase.auth.FirebaseUser;
-
-import java.util.ArrayList;
 
 import java.util.List;
 import java.util.concurrent.Executor;
@@ -24,15 +18,10 @@ public class Model {
     private Model() {
     }
 
-
     private Executor executor = Executors.newSingleThreadExecutor();
     private Handler mainHandler = HandlerCompat.createAsync(Looper.getMainLooper());
 
-    public interface Listener<T>{
-        void onComplete(T data);
-    }
-
-    public static Model instance(){
+    public static Model instance() {
         return _instance;
     }
 
@@ -71,31 +60,8 @@ public class Model {
     }
 
 
-
-    public void signUpUser(String name, String email ,String password,Pet pet, Listener<Void> listener){
-        firebaseModel.signUpUser(email,password, (FireBaseUser)->{
-            Log.d("TAG", FireBaseUser.getUid());
-
-            User user = new User(name,email,password,pet);
-            user.setUserFirebaseID(FireBaseUser.getUid());
-            Log.d("TAG", user.toJson().toString());
-
-            firebaseModel.addUser(user,(unused)->{
-                listener.onComplete(null);
-            });
-        });
-    }
-
-    public void signInUser(String email, String password, Listener<Void> listener){
-        firebaseModel.signInUser(email,password,(FireBaseUser)->{
-            Log.d("TAG", FireBaseUser.getUid());
-            firebaseModel.getUser(FireBaseUser.getUid(), (User)->{
-                Log.d("TAG", "userfound in Model");
-                listener.onComplete(null);
-            });
-
-        });
-
+    public void signUpUser(String email, String password) {
+        firebaseModel.signUpUser(email, password);
     }
 
 }
