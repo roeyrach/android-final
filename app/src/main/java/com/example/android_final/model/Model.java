e6a83f5f0602b9e683f2ccf896da9f25c098c84d
roeyrach <93585090+roeyrach@users.noreply.github.com>
2023-02-13 18:43:45 +0200

    adding localDB

--- app/src/main/java/com/example/android_final/model/Model.java
+++ app/src/main/java/com/example/android_final/model/Model.java
@@ -1,7 +1,13 @@
 package com.example.android_final.model;
 
-import java.util.ArrayList;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.core.os.HandlerCompat;
+
 import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class Model {
     private static final Model _instance = new Model();
@@ -9,29 +15,53 @@ public class Model {
     private FirebaseModel firebaseModel = new FirebaseModel();
 
 
-    private Model(){
+    private Model() {
     }
 
-    public static Model instance(){
+    private Executor executor = Executors.newSingleThreadExecutor();
+    private Handler mainHandler = HandlerCompat.createAsync(Looper.getMainLooper());
+
+    public static Model instance() {
         return _instance;
     }
 
-    List<Post> postsList = new ArrayList<>();
+    AppLocalDbRepository localDb = AppLocalDb.getAppDb();
 
-    public List<Post> getAllPosts() {
-        for (int i = 0; i < 10; i++) {
-            postsList.add(new Post("Post " + i, "Hello guy how you doin'?", ""));
-        }
-
-        return postsList;
-    }
-    public void addPost(Post post){
-        postsList.add(post);
+    public interface GetAllPostsListener {
+        void onComplete(List<Post> data);
     }
 
+    public void getAllPosts(GetAllPostsListener callback) {
+        executor.execute(() -> {
+            List<Post> data = localDb.postDao().getAll();
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            mainHandler.post(() -> callback.onComplete(data));
+        });
+    }
 
-    public void signUpUser(String email, String password){
-        firebaseModel.signUpUser(email,password);
+    public interface AddPostListener {
+        void onComplete();
+    }
+
+    public void addPost(Post p, AddPostListener listener) {
+        executor.execute(() -> {
+            localDb.postDao().insertAll(p);
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            mainHandler.post(() -> listener.onComplete());
+        });
+    }
+
+
+    public void signUpUser(String email, String password) {
+        firebaseModel.signUpUser(email, password);
     }
 
 }
