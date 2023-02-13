eac7469cff67ff9a3f65dcfd6b83625b7f7919f8
roeyrach <93585090+roeyrach@users.noreply.github.com>
2023-02-13 19:09:43 +0200

    Revert "Merge pull request #26 from GuyBarzily/Roey"
    This reverts commit ffa2e63e4324be8f0880cafb34bf41ea745e164c, reversing
    changes made to 005150f1d3af8b966074c3f071fa0e178a1e9532.

--- app/src/main/java/com/example/android_final/SignInFragment.java
+++ app/src/main/java/com/example/android_final/SignInFragment.java
@@ -5,12 +5,14 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.android_final.databinding.FragmentSignInBinding;
 import com.example.android_final.databinding.FragmentSignUpBinding;
+import com.example.android_final.model.Model;
 
 
 public class SignInFragment extends Fragment {
@@ -32,7 +34,11 @@ public class SignInFragment extends Fragment {
             NavHostFragment.findNavController(SignInFragment.this).navigate(R.id.action_signInFragment_to_signUpFragment);
         });
         binding.signInBtn.setOnClickListener(view->{
-            NavHostFragment.findNavController(SignInFragment.this).navigate(R.id.action_signInFragment_to_mainFeedFragment);
+            Model.instance().signInUser("Guy@guy.guy", "123456",(unused)->{
+                Log.d("TAG", "userFound");
+                NavHostFragment.findNavController(SignInFragment.this).navigate(R.id.action_signInFragment_to_mainFeedFragment);
+            });
+
         });
         // Inflate the layout for this fragment
         return binding.getRoot();
