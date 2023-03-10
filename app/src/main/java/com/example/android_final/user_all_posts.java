114833620aa5144185cc206141edd966b35e9789
roeyrach <93585090+roeyrach@users.noreply.github.com>
2023-03-10 11:58:06 +0200

    user_all_posts

--- app/src/main/java/com/example/android_final/user_all_posts.java
+++ app/src/main/java/com/example/android_final/user_all_posts.java
@@ -1,31 +1,77 @@
 package com.example.android_final;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.android_final.databinding.FragmentUserAllPostsBinding;
+import com.example.android_final.model.Model;
+import com.example.android_final.model.Post;
+
+import java.util.List;
 
 
 public class user_all_posts extends Fragment {
 
     FragmentUserAllPostsBinding binding;
-
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
+    PostRecyclerAdapter adapter;
+    PostsListFragmentViewModel viewModel;
+    UserViewModel userViewModel;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_all_posts, container, false);
+        binding = FragmentUserAllPostsBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+
+        ActionBar actionBar = ((AppCompatActivity) getActivity()).getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        binding.userAllPostsPostsRecyclerList.setHasFixedSize(true);
+        binding.userAllPostsPostsRecyclerList.setLayoutManager(new LinearLayoutManager(getContext()));
+        adapter = new PostRecyclerAdapter(getLayoutInflater(), viewModel.getData().getValue());
+        binding.userAllPostsPostsRecyclerList.setAdapter(adapter);
+
+        userViewModel = new ViewModelProvider(this).get(UserViewModel.class);
+        userViewModel.getCurrentUser().observe(getViewLifecycleOwner(),user -> {
+            if (user != null) {
+                viewModel.getData().observe(getViewLifecycleOwner(), list -> {
+                    Model.instance().getAllUserPosts(userViewModel.getCurrentUser().getValue().getUserFirebaseID(), posts -> {
+                        adapter.setData(posts);
+                    });
+                });
+            }
+        });
+
+        return view;
     }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        viewModel = new ViewModelProvider(this).get(PostsListFragmentViewModel.class);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
 }
+
+
